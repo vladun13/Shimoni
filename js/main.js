96c2b68562ca8af3c5533f55588e6f67086a18ba
vladun13 <nemyrovsky1311vlad@gmail.com>
2018-12-02 10:01:27 +0200

    Shimoni

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    /* SLIDER */
+    $(".slider__list").slick({
+			autoplay: true,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			rtl: true,
+			arrows: true,
+			appendArrows: $('.slider__arrows'),
+			prevArrow: '<button type="button" class="slider__arrow slider__arrow_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+			nextArrow: '<button type="button" class="slider__arrow slider__arrow_next"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+			dots: true,
+			appendDots: $('.slider__dots'),
+			dotsClass: 'slider__slick-dots'
+	});
+
+	    /* Scroll menu */
+			function swimMenu(){
+        var navFixed = $(".phone-box.navigation__nofixed"),
+        navHeight = navFixed.height();
+    
+        if ($(window).scrollTop()) {
+            navFixed.addClass("navigation__fixed").removeClass("navigation__nofixed");
+        };
+    
+        $(window).scroll(function(){
+            var scroll = $(window).scrollTop();
+            
+            if (scroll < navHeight + 500) {
+                navFixed.addClass("navigation__nofixed");
+                navFixed.removeClass("navigation__fixed navigation__hidden");
+            };
+            if (scroll > navHeight){
+                navFixed.addClass("navigation__hidden");
+            };
+            if (scroll > navHeight + 500){
+                navFixed.addClass("navigation__fixed");
+                navFixed.removeClass("navigation__nofixed");
+						};
+						
+        });
+    };
+
+    swimMenu();
+
+
+});
+
